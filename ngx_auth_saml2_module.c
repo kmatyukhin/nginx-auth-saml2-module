7f41b9b1640e587e7581c73a0b8f98d94d0a23e0
Konstantin Matyukhin <kmatyukhin@gmail.com>
2018-03-25 21:00:01 +0200

    Add sructure for module configuration

--- ngx_auth_saml2_module.c
+++ ngx_auth_saml2_module.c
@@ -12,36 +12,44 @@
 #include <lasso/lasso.h>
 
 
+typedef struct {
+    ngx_http_complex_value_t   idp_metadata;
+} ngx_auth_saml2_loc_conf_t;
+
+
+static void *ngx_auth_saml2_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_auth_saml2_merge_loc_conf(ngx_conf_t *cf,
+    void *parent, void *child);
 static ngx_int_t ngx_auth_saml2_worker_init(ngx_cycle_t *cycle);
 static void ngx_auth_saml2_worker_exit(ngx_cycle_t *cycle);
 static char *ngx_auth_saml2(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 
-static ngx_command_t ngx_auth_saml2_commands[] = {
+static ngx_command_t  ngx_auth_saml2_commands[] = {
 
     { ngx_string("auth_saml2_idp_metadata"),
       NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_CONF_TAKE1,
-      ngx_auth_saml2, /* configuration setup function */
-      0, /* No offset. Only one context is supported. */
-      0, /* No offset when storing the module configuration on struct. */
+      ngx_auth_saml2,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_auth_saml2_loc_conf_t, idp_metadata),
       NULL },
 
-    ngx_null_command /* command termination */
+    ngx_null_command
 };
 
 
-static ngx_http_module_t ngx_auth_saml2_module_ctx = {
-    NULL, /* preconfiguration */
-    NULL, /* postconfiguration */
+static ngx_http_module_t  ngx_auth_saml2_module_ctx = {
+    NULL,                            /* preconfiguration */
+    NULL,                            /* postconfiguration */
 
-    NULL, /* create main configuration */
-    NULL, /* init main configuration */
+    NULL,                            /* create main configuration */
+    NULL,                            /* init main configuration */
 
-    NULL, /* create server configuration */
-    NULL, /* merge server configuration */
+    NULL,                            /* create server configuration */
+    NULL,                            /* merge server configuration */
 
-    NULL, /* create location configuration */
-    NULL  /* merge location configuration */
+    ngx_auth_saml2_create_loc_conf,  /* create location configuration */
+    ngx_auth_saml2_merge_loc_conf    /* merge location configuration */
 };
 
 
@@ -59,6 +67,34 @@ ngx_module_t ngx_auth_saml2_module = {
     NULL,                            /* exit master */
     NGX_MODULE_V1_PADDING
 };
+
+
+static void *
+ngx_auth_saml2_create_loc_conf(ngx_conf_t *cf)
+{
+    ngx_auth_saml2_loc_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_auth_saml2_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    return conf;
+}
+
+
+static char *
+ngx_auth_saml2_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+    ngx_auth_saml2_loc_conf_t  *prev = parent;
+    ngx_auth_saml2_loc_conf_t  *conf = child;
+
+    if (conf->idp_metadata.value.data == NULL) {
+        conf->idp_metadata = prev->idp_metadata;
+    }
+
+    return NGX_CONF_OK;
+}
 
 
 static ngx_int_t
